6303a9c04e5962007d1f830a6de2671b00a296fa
Joseph Ahmed <94310209+ahmedjoseph07@users.noreply.github.com>
2024-04-21 17:25:59 +0600

    Update foodRoutes.js

--- server/routes/foodRoutes.js
+++ server/routes/foodRoutes.js
@@ -1,20 +1,18 @@
-// routes/foodRoutes.js
-
 import express from 'express';
 import { createFood, getAllFoods, updateFood, deleteFood } from '../controllers/foodController.js';
 
 const router = express.Router();
 
-// Create a new food item
+
 router.post('/', createFood);
 
-// Get all food items
+
 router.get('/', getAllFoods);
 
-// Update a food item by ID
+
 router.put('/:id', updateFood);
 
-// Delete a food item by ID
+
 router.delete('/:id', deleteFood);
 
 export default router;
